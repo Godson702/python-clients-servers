c189f034f81e211eb508d3fa3436067e95158ff8
Godson702 <e2378353@cmaisonneuve.qc.ca>
2025-10-24 15:43:03 -0400

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -7,25 +7,19 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('142.251.41.78', 80)
+server_address = ('10.20.61.14', 10000)
 print('Connexion TCP a %s sur le port %s' % server_address)
 sock.connect(server_address)
 
 try:
     
     # Send data
-    message = 'GET / HTTP/1.0\r\n Host: google.com\r\n\r\n'
+    #message = 'GET / HTTP/1.0\r\n Host: google.com\r\n\r\n'
+    message = 'allumer_LED'
     print('Envoi de : "%s"' % message)
     sock.sendall(message.encode())
     
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(1024)
-        amount_received += len(data)
-        print('Recu :  "%s"' % data.decode())
+
 
 finally:
     print('Fermeture du socket TCP')
